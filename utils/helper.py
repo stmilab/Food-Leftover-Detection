cb814bd9a37b3056fda1969c34d19813f714307e
Innoversa <sicongjaxhuang@gmail.com>
2025-02-08 19:07:06 -0600

    adding helper function

---
+++ utils/helper.py
@@ -0,0 +1,26 @@
+import numpy as np
+import torch
+
+
+# NOTE: custom min-max normalization class that works on numpy and tensor
+class Sicong_Norm:
+    def __init__(self, arr=None, min_val: float = 0, max_val: float = 200):
+        if arr is None:
+            self.min = min_val
+            self.max = max_val
+        else:
+            # Making sure input is numpy
+            if isinstance(arr, torch.Tensor):
+                arr = arr.cpu().numpy()
+            self.min = arr.min()
+            self.max = arr.max()
+
+    def normalize(self, arr):
+        return (arr - self.min) / (self.max - self.min)
+
+    def denormalize(self, arr):
+        return arr * (self.max - self.min) + self.min
+
+    # NOTE: alias for normalize and denormalize
+    norm = normalize
+    denorm = denormalize
